9b898b2c8cdc377a6f20ce45b7468a974d86acca
Nina Palma <ninapalmadev@gmail.com>
2022-09-29 15:16:54 -0300

    faz primeira conexão com SQL

---
+++ src/br/com/alura/TestaConexao.java
@@ -0,0 +1,16 @@
+package br.com.alura;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+	public static void main(String[] args) throws SQLException {
+		Connection connection = DriverManager.getConnection(
+				"jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "54321");
+		
+		System.out.println("Fechando conexão");
+		
+		connection.close();
+	}
+}
