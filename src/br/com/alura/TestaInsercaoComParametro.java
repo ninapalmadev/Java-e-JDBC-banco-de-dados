aa22b3fcfe17dd2b7f7edd28803548520238a72d
Nina Palma <ninapalmadev@gmail.com>
2022-09-29 18:21:04 -0300

    cria controle de autocommit

--- src/br/com/alura/TestaInsercaoComParametro.java
+++ src/br/com/alura/TestaInsercaoComParametro.java
@@ -10,22 +10,34 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
 
-		String nome = "LED";
-		String descricao = "LED 5038); delete from Produto;";
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
-				
-		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO(nome, descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
-		
-		stm.setString(1,  nome);
+		connection.setAutoCommit(false);
+
+		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO(nome, descricao) VALUES (?,?)",
+				Statement.RETURN_GENERATED_KEYS);
+
+		adicionarVariavel("SmartTV", "80 polegadas", stm);
+		adicionarVariavel("Led", "Led 5038", stm);
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
+		stm.setString(1, nome);
 		stm.setString(2, descricao);
-		
+
+//		if (nome.equals("Led")) {
+//			throw new RuntimeException("Nao foi possivel add produto");
+//		}
+
 		stm.execute();
 
 		ResultSet rst = stm.getGeneratedKeys();
+		
 		while (rst.next()) {
 			Integer id = rst.getInt(1);
 			System.out.println("O id criado foi: " + id);
 		}
+		
+		rst.close();
 	}
 }
