07f259355c452f8d018b9bfea6bf6793422639a4
Nina Palma <ninapalmadev@gmail.com>
2022-09-30 14:08:45 -0300

    lista produtos usando DAO

--- src/TestaInsercaoEListagemComProduto.java
+++ src/TestaInsercaoEListagemComProduto.java
@@ -1,23 +1,22 @@
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import br.com.alura.ConnectionFactory;
 import br.com.alura.dao.ProdutoDAO;
 import br.com.alura.modelo.Produto;
 
-public class TestaInsercaoComProduto {
+public class TestaInsercaoEListagemComProduto {
 
 	public static void main(String[] args) throws SQLException {
 
 		Produto comoda = new Produto("Comoda", "Comoda vertical");
 
 		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
-			ProdutoDAO persistenciaProduto = new ProdutoDAO(connection);
-			persistenciaProduto.salvar(comoda);
-			//Lista = persistenciaProduto.listar();
+			ProdutoDAO produtoDAO = new ProdutoDAO(connection);
+			produtoDAO.salvar(comoda);
+			List<Produto> listaDeProdutos = produtoDAO.listar();
+			listaDeProdutos.stream().forEach(lp -> System.out.println(lp));
 		}
-
-		System.out.println(comoda);
 	}
-
 }
--- src/br/com/alura/modelo/Produto.java
+++ src/br/com/alura/modelo/Produto.java
@@ -11,6 +11,13 @@ public class Produto {
 		this.nome = nome;
 		this.descricao = descricao;
 	}
+	public Produto(Integer id, String nome, String descricao) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+	
+	
 
 	public String getNome() {
 		return nome;
@@ -25,7 +32,7 @@ public class Produto {
 	
 	@Override
 	public String toString() {
-		return String.format("O produto criado foi: %d, %s, %s", 
+		return String.format("O produto eh: %d, %s, %s", 
 				this.id, this.nome, this.descricao);
 	}
 }
